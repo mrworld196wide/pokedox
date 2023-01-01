4983d4ef983224e620d4c7542c6c575b3ee013c7
mrworld196wide <shubhum19@gmail.com>
2023-01-01 15:14:06 +0530

    Pokecard component created

---
+++ src/Pokecard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './Pokecard.css';
+const POKEAPI= 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+class Pokecard extends Component {
+    render() {
+        let imgSrc= `${POKEAPI}${this.props.id}.png`;
+        return (
+
+            <div className='Pokecard'>
+                <h1>{this.props.name} </h1>
+                <img src={imgSrc} alt="" />
+                <div>Type: {this.props.type}</div>
+                <div>EXP: {this.props.exp}</div>
+            </div>
+        )
+    }
+}
+
+export default Pokecard;
